2c4e82e522f5002b7b79b2cdd7c70447a967049c
charrod <carter.harrod283@gmail.com>
2022-05-09 22:35:52 -0500

    init-java

---
+++ src/main/java/com/charrod/enchantingrevisited/EnchantingRevisited.java
@@ -0,0 +1,17 @@
+package com.charrod.enchantingrevisited;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnchantingRevisited extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
